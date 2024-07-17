ad40e3b2981f288dc2b419764512bfc6c31e6511
agcoelho <afonsogcoelho@gmail.com>
2024-07-17 15:14:29 -0400

    adicionando contador de chances

--- hangman.py
+++ hangman.py
@@ -3,20 +3,36 @@ print('wellcome to the hangman game!!')
 print('###############################')
 
 secret_word = 'abacate'
+correct = ["_", "_", "_", "_", "_", "_", "_",]
 dead = False
 right = False
+c = 6
 
 
 
 while not dead and not right:
     print('jogando...\n')
-    c = 6
+    
     guess = input('Guess one character\n')
-    index = 0
-    for character in secret_word:
-        if guess == character:
-            print('The secret word has {} in the position: {}'.format(character, index+1))
-        index = index + 1
+    
+    
+    if guess in secret_word:
+        
+        index = 0
+        for character in secret_word:
+            if guess.upper() == character.upper():
+                correct[index] = character
+            index = index + 1
+    else:
+        print('theres no such character.\nTry again.')
+        c = c - 1
+        print('you still have {} try\n'.format(c))
+        if c < 1:
+            dead = True
+            print('a palavra era: {}'.format(secret_word))
+
+    if not dead:    
+        print('{}\n'.format(correct))
         
         
 
